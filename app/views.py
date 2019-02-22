342bb63d52365090937a7fe0d32b4ab4ded77b66
Diandra Whittick <diandra.whittick@mymona.uwi.edu>
2019-02-22 01:41:03 +0000

    Exercise one Completed

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from app.forms import UploadForm
 
 
 ###
@@ -28,19 +29,23 @@ def about():
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
+    
     if not session.get('logged_in'):
         abort(401)
 
     # Instantiate your form class
-
-    # Validate file upload on submit
-    if request.method == 'POST':
-        # Get file data and save to your uploads folder
-
+    form = UploadForm()
+    #if form.validate_on_submit(): # Validate file upload on submit
+    if request.method == 'POST' and form.validate_on_submit():
+        #Get file data and save to your uploads folder
+        photo = form.photo.data
+        filename = secure_filename(photo.filename)
+        photo.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
         flash('File Saved', 'success')
         return redirect(url_for('home'))
+    flash_errors(form)
 
-    return render_template('upload.html')
+    return render_template('upload.html',form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
